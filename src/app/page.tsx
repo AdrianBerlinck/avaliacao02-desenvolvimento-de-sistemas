b43bb6701a15311fb2e4206a65d620d4619374d5
ADRIAN DA SILVA BERLINCK <adrian_berlinck@estudante.sc.senai.br>
2025-04-24 21:33:09 -0300

    prova

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,82 @@
-import { useEffect } from 'react';
+'use client'
+import { FormEvent, useState, useEffect } from "react";
+import axios from "axios";
+import { v4 as uuid } from 'uuid';
 
-export default function MarketList() {
+type Item = {
+  id: string;
+  text: string;
+  checked: boolean;
+}
+
+export default function MakeList() {
+
+  const [item, setItem] = useState('');
+  const [itens, setItens] = useState<Item[]>([]);
+
+  const disabled_button = !item;
+
   useEffect(() => {
-    loadItens();
-  }, [])
+    async function fetchItens() {
+      try {
+        const response = await axios.get("http://localhost:3001/itens");
+        setItens(response.data);
+      } catch (error) {
+        console.error("Erro ao buscar os itens:", error);
+      }
+    }
 
+    fetchItens();
+  }, []);
 
-  function loadItens() {
-    // BUSCA AS INFORMAÇÕES NA API FAKE
-    // SALVA O VALOR NO ESTADO
-  }
+  async function HandleSubmit(event: FormEvent) {
+    event.preventDefault();
 
-  function handleAddItem() {
-    // CRIAR O OBJETO DO ITEM
-    // CHAMA A API PARA ADICIONAR O ITEM
-    // CARREGA OS PRODUTOS NOVAMENTE // loadItens();
-  }
+    const novoItem: Item = {
+      id: uuid(),
+      text: item,
+      checked: false
+    };
 
-  function handleRemoveItem(id: string) {
-    // FILTRA O ESTADO E REMOVE O ITEM
-    // CHAMA A API PARA REMOVER O ITEM
-    // CARREGA OS PRODUTOS NOVAMENTE // loadItens();
-  }
-
-  function handleUpdateItem(id: string) {
-    // CRIA O OBJETO DO ITEM
-    // CHAMA A API PARA ATUALIZAR O ITEM
-    // CARREGA OS PRODUTOS NOVAMENTE // loadItens();
+    try {
+      await axios.post("http://localhost:3001/itens", novoItem);
+      setItens(prev => [...prev, novoItem]);
+      setItem('');
+    } catch (error) {
+      console.error('Erro ao enviar produto:', error);
+    }
   }
 
   return (
-    <h1>MarketList</h1>
+    <div>
+      <div>
+        <form onSubmit={HandleSubmit}>
+          <h1>MakeList</h1>
+          <div className='input'>
+            <input
+              type="text"
+              id='item'
+              placeholder='Informe o item'
+              value={item}
+              onChange={(e) => setItem(e.target.value)}
+              required
+            />
+          </div>
+          <button type="submit" className='button-cadastrar' disabled={disabled_button}>
+            Salvar
+          </button>
+        </form>
+      </div>
+
+      <h1>Lista</h1>
+      <ul>
+        {itens.map((it) => (
+          <li key={it.id}>
+            <input type="checkbox" />
+            {it.text}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
